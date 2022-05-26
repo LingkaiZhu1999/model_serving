baf6b2b37316bd5962d42a2b304fcae64b52a109
LingkaiZhu1999 <zhulingkai123@gmail.com>
2022-05-26 20:31:50 +0200

    add decision tree model

--- data.py
+++ data.py
@@ -6,7 +6,10 @@ import os
 import urllib
 import csv
 import numpy as np
+import pandas
+from sklearn.model_selection import train_test_split
 
+dataset = pandas.read_csv('data.csv')
 def commit_count(project, sha='main', token='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'):
     """
     Return the number of commits to a project
@@ -37,56 +40,66 @@ def commit_count(project, sha='main', token='ghp_yG8YIpZiA45fNyOrj9T26FAXNXUIrs3
         commit_count = int(dict(urllib.parse.parse_qsl(qs))['page'])
     return commit_count
 
-def get_data():
+def get_data(page, per_page, length):
     token = os.getenv('GITHUB_TOKEN', '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')
     query = "stars:>=50"
     order = "order=desc"
-    page = "page=10"
-    per_page = "per_page=100"
-    query_url = f"https://api.github.com/search/repositories?q={query}&{order}&{page}&{per_page}"
+    page_ = "page="+str(page)
+    per_page_ = "per_page="+str(per_page)
+    query_url = f"https://api.github.com/search/repositories?q={query}&{order}&{page_}&{per_page_}"
     headers = {'Authorization': f'token {token}'}
     r = requests.get(query_url, headers=headers)
+    print(r.json())
     repos = r.json()['items']
     while 'next' in r.links.keys():
         r = requests.get(r.links['next']['url'], headers=headers)
         repos.extend(r.json()['items'])
-        if len(repos) == 1000:
+        if len(repos) == length:
             break
     data_ = []
     for repo in repos:
-        data = dict({'commits': None, 'forks': None, 'watchers': None, 'size': None, 'open_issue': None, 'star': None})
+        data = {}
         default_branch = repo['default_branch']
         repo_url = repo['url']
         num_commit = commit_count(repo_url, default_branch)
         repo['num_commit'] = num_commit
 
-        fork = int(repo['forks'])
-        watcher = int(repo['watchers'])
-        star_gazers = int(repo['stargazers_count'])
-        size = int(repo['size'])
-        open_issue = int(repo['open_issues'])
+        # fork = int(repo['forks'])
+        # watcher = int(repo['watchers'])
+        # star_gazers = int(repo['stargazers_count'])
+        # size = int(repo['size'])
+        # open_issue = int(repo['open_issues'])
         # data['commits'].append(num_commit)
         # data['forks'].append(fork)
         # data['watchers'].append(watcher)
         # data['star'].append(star_gazers)
         # data['size'].append(size)
         # data['open_issue'].append(open_issue)
-        data['commits'] = num_commit
-        data['forks'] = fork
-        data['watchers'] = watcher
-        data['star'] = star_gazers
-        data['size'] = size
-        data['open_issue'] = open_issue
+        # data['commits'] = num_commit
+        # data['forks'] = fork
+        # data['watchers'] = watcher
+        # data['star'] = star_gazers
+        # data['size'] = size
+        # data['open_issue'] = open_issue
+        # data_.append(data)
+        for key, value in repo.items():
+            if type(value) in [int, float, bool]:
+                data[key] = value
         data_.append(data)
+
+
+
     return data_
 
+
 def main():
-    repos = get_data()
-    print(repos)
+    repos_training = get_data(page=1, per_page=100, length=1000)
     with open('data.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=repos[0].keys())
+        writer = csv.DictWriter(csvfile, fieldnames=repos_training[0].keys())
         writer.writeheader()
-        writer.writerows(repos)
+        writer.writerows(repos_training)
+
+
 
 if __name__=="__main__":
     main()
--- sklearn_model.py
+++ sklearn_model.py
@@ -2,15 +2,20 @@ import pandas
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
+import pickle
+
 # load the dataset
-dataset = pandas.read_csv('data.csv')
+dataset = pandas.read_csv('training_data.csv')
 # split into input (X) and output (y) variables
-X = dataset.drop(columns=['star', 'watchers'])
-y = dataset['star']
+X = dataset.drop(columns=['stargazers_count', 'id', 'private'])
+y = dataset['stargazers_count']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 decision_tree_regressor = DecisionTreeRegressor(random_state=0)
 decision_tree_regressor.fit(X_train, y_train)
 y_pred = decision_tree_regressor.predict(X_test)
 accuracy = np.mean((y_pred-y_test)**2)
 print(accuracy)
-print(y_pred, y_test)
+model_file_name = "decision_tree.pkl"
+with open(model_file_name, 'wb') as file:
+    pickle.dump(decision_tree_regressor, file)
+
