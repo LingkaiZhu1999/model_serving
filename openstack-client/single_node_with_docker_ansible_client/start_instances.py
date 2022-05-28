6260fc99a56c0bdf2388aa901a110ddd4029b22d
Ubuntu <zhulingkai123@gmail.com>
2022-05-28 15:50:12 +0000

    update start instances

--- openstack-client/single_node_with_docker_ansible_client/start_instances.py
+++ openstack-client/single_node_with_docker_ansible_client/start_instances.py
@@ -3,22 +3,23 @@ import time, os, sys, random, re
 import inspect
 from os import environ as env
 
-from  novaclient import client
+from novaclient import client
 import keystoneclient.v3.client as ksclient
 from keystoneauth1 import loading
 from keystoneauth1 import session
 
 
-flavor = "ssc" 
-private_net = "SNIC Network"
+flavor = "ssc.medium"
+private_net = "UPPMAX 2022/1-1 Internal IPv4 Network"
 floating_ip_pool_name = None
 floating_ip = None
-image_name = "image-ID"
+image_name = "Ubuntu 20.04 - 2021.03.23"
 
-identifier = random.randint(1000,9999)
+identifier = 1
 
 loader = loading.get_plugin_loader('password')
 
+key_name = 'LingkaiZhu'
 auth = loader.load_from_options(auth_url=env['OS_AUTH_URL'],
                                 username=env['OS_USERNAME'],
                                 password=env['OS_PASSWORD'],
@@ -34,6 +35,7 @@ print ("user authorization completed.")
 image = nova.glance.find_image(image_name)
 
 flavor = nova.flavors.find(name=flavor)
+
 
 if private_net != None:
     net = nova.neutron.find_network(private_net)
@@ -55,25 +57,36 @@ if os.path.isfile(cfg_file_path):
 else:
     sys.exit("dev-cloud-cfg.txt is not in current working directory")    
 
-secgroups = ['default']
+secgroups = ['Lingkai']
 
 print ("Creating instances ... ")
-instance_prod = nova.servers.create(name="prod_server_with_docker_"+str(identifier), image=image, flavor=flavor, key_name='<KEY-NAME>',userdata=userdata_prod, nics=nics,security_groups=secgroups)
-instance_dev = nova.servers.create(name="dev_server_"+str(identifier), image=image, flavor=flavor, key_name='<KEY-NAME>',userdata=userdata_dev, nics=nics,security_groups=secgroups)
+instance_prod = nova.servers.create(name="prod_server_with_docker_"+str(identifier), image=image, flavor=flavor, key_name=key_name,userdata=userdata_prod, nics=nics,security_groups=secgroups)
+instance_dev_1 = nova.servers.create(name="dev_server_lingkai"+str(identifier), image=image, flavor=flavor, key_name=key_name,userdata=userdata_dev, nics=nics,security_groups=secgroups)
+instance_dev_2 = nova.servers.create(name="dev_server_lingkai"+str(identifier+1), image=image, flavor=flavor, key_name=key_name,userdata=userdata_dev, nics=nics,security_groups=secgroups)
+instance_dev_3 = nova.servers.create(name="dev_server_lingkai"+str(identifier+2), image=image, flavor=flavor, key_name=key_name,userdata=userdata_dev, nics=nics,security_groups=secgroups)
 inst_status_prod = instance_prod.status
-inst_status_dev = instance_dev.status
+inst_status_dev_1 = instance_dev_1.status
+inst_status_dev_2 = instance_dev_2.status
+inst_status_dev_3 = instance_dev_3.status
+
 
 print ("waiting for 10 seconds.. ")
 time.sleep(10)
 
-while inst_status_prod == 'BUILD' or inst_status_dev == 'BUILD':
+while inst_status_prod == 'BUILD' or inst_status_dev_1 == 'BUILD' or inst_status_dev_2 == 'BUILD' or inst_status_dev_3 == 'BUILD':
     print ("Instance: "+instance_prod.name+" is in "+inst_status_prod+" state, sleeping for 5 seconds more...")
-    print ("Instance: "+instance_dev.name+" is in "+inst_status_dev+" state, sleeping for 5 seconds more...")
+    print ("Instance: "+instance_dev_1.name+" is in "+inst_status_dev_1+" state, sleeping for 5 seconds more...")
+    print("Instance: " + instance_dev_2.name + " is in " + inst_status_dev_2 + " state, sleeping for 5 seconds more...")
+    print("Instance: " + instance_dev_3.name + " is in " + inst_status_dev_3 + " state, sleeping for 5 seconds more...")
     time.sleep(5)
     instance_prod = nova.servers.get(instance_prod.id)
     inst_status_prod = instance_prod.status
-    instance_dev = nova.servers.get(instance_dev.id)
-    inst_status_dev = instance_dev.status
+    instance_dev_1 = nova.servers.get(instance_dev_1.id)
+    inst_status_dev_1 = instance_dev_1.status
+    instance_dev_2 = nova.servers.get(instance_dev_2.id)
+    inst_status_dev_2 = instance_dev_2.status
+    instance_dev_3 = nova.servers.get(instance_dev_3.id)
+    inst_status_dev_3 = instance_dev_3.status
 
 ip_address_prod = None
 for network in instance_prod.networks[private_net]:
@@ -83,13 +96,31 @@ for network in instance_prod.networks[private_net]:
 if ip_address_prod is None:
     raise RuntimeError('No IP address assigned!')
 
-ip_address_dev = None
-for network in instance_dev.networks[private_net]:
+ip_address_dev_1 = None
+for network in instance_dev_1.networks[private_net]:
     if re.match('\d+\.\d+\.\d+\.\d+', network):
-        ip_address_dev = network
+        ip_address_dev_1 = network
         break
-if ip_address_dev is None:
+if ip_address_dev_1 is None:
+    raise RuntimeError('No IP address assigned!')
+
+ip_address_dev_2 = None
+for network in instance_dev_2.networks[private_net]:
+    if re.match('\d+\.\d+\.\d+\.\d+', network):
+        ip_address_dev_2 = network
+        break
+if ip_address_dev_2 is None:
+    raise RuntimeError('No IP address assigned!')
+
+ip_address_dev_3 = None
+for network in instance_dev_3.networks[private_net]:
+    if re.match('\d+\.\d+\.\d+\.\d+', network):
+        ip_address_dev_3 = network
+        break
+if ip_address_dev_3 is None:
     raise RuntimeError('No IP address assigned!')
 
 print ("Instance: "+ instance_prod.name +" is in " + inst_status_prod + " state" + " ip address: "+ ip_address_prod)
-print ("Instance: "+ instance_dev.name +" is in " + inst_status_dev + " state" + " ip address: "+ ip_address_dev)
+print ("Instance: "+ instance_dev_1.name +" is in " + inst_status_dev_1 + " state" + " ip address: "+ ip_address_dev_1)
+print ("Instance: "+ instance_dev_2.name +" is in " + inst_status_dev_2 + " state" + " ip address: "+ ip_address_dev_2)
+print ("Instance: "+ instance_dev_3.name +" is in " + inst_status_dev_3 + " state" + " ip address: "+ ip_address_dev_3)
