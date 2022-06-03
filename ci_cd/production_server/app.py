47cc76dbb76d6248610f8f3a90cba75d66219cfe
LingkaiZhu1999 <zhulingkai123@gmail.com>
2022-06-03 20:15:24 +0000

    merge code from manne

--- ci_cd/production_server/app.py
+++ ci_cd/production_server/app.py
@@ -8,10 +8,22 @@ from workerA import get_predictions, load_model, get_accuracy
 from fetch_data import GithubCrawler
 import validators
 import pandas as pd
-
+from inspect import getmodule
+from save_model import ModelWrapper
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'testing'
+
+model_wrapper = ModelWrapper.loadModel("best_model")
+
+if model_wrapper.metrics:
+    for key, value in model_wrapper.metrics.items():
+        print(type(value))
+        if type(value) == numpy.float64:
+            if value > 1:
+                model_wrapper.metrics[key] = round(value)
+            else:
+                model_wrapper.metrics[key] = round(value, 3)
 
 bot = GithubCrawler(token="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")
 
@@ -19,7 +31,7 @@ bot = GithubCrawler(token="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")
 @app.route("/", methods=["GET"])
 def index():
     """Home page"""
-    return render_template("index.html")
+    return render_template("index.html", metrics=model_wrapper.metrics)
 
 
 @app.route("/", methods=["POST"])
@@ -28,19 +40,19 @@ def predict():
     url = request.form["repo_url"]
     if not validators.url(url):
         print("Invalid url")
-        return render_template("index.html", error="Invalid url")
+        return render_template("index.html", error="Invalid url", metrics=model_wrapper.metrics)
 
     repo = bot.fetchRepoFromURL(url)
 
     if type(repo) != pd.DataFrame:
-        return render_template("index.html", error="Repository not found")
+        return render_template("index.html", error="Repository not found", metrics=model_wrapper.metrics)
 
     ### Load model ###
     # model = loadModel()
     prediction = get_predictions(repo)
     accuracy = get_accuracy(repo)
     actual = repo["stargazers_count"]
-    return render_template("index.html", result={"prediction": prediction, "actual": actual.values[0], "accuracy": accuracy})
+    return render_template("index.html", result={"prediction": prediction, "actual": actual.values[0], "accuracy": accuracy},metrics=model_wrapper.metrics)
 
 
 if __name__ == "__main__":
